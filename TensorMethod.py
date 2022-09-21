fixed normalization in single

TensorMethod.py
@@ -21,7 +21,7 @@ fs=12
 #%%
 # Read image and normalize
 I=np.float32(io.imread(filename))
-I=(I-np.min(I))/np.max(I)-np.min(I)
+I=(I-np.min(I))/(np.max(I)-np.min(I))
 
 # Using tensor method to calculate the phase
 Ix = I[2:,1:-1]-I[0:-2,1:-1]